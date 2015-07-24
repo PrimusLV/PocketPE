bcfc67949c40df224ddeeb7c3d53da216cec9fdc
DuncanA <mccleanduncan03@hotmail.com>
2015-07-24 15:58:02 +0100

    Create list.php

---
+++ src/PocketPE/commands/list.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PocketPE\commands\list;
+
+use pocketPE\plugins\Core;
+
+class List extends Core {
+ >List()Create
+    help - shows help for player
+    debug - shows tps and other debugging tools
+}
