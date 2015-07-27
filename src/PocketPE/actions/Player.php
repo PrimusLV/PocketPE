1fa858f7449c8702f4e7a5fab04ef70454dd8091
DuncanA <mccleanduncan03@hotmail.com>
2015-07-27 09:59:18 +0100

    Create Player.php

---
+++ src/PocketPE/actions/Player.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PocketPE\actions\Player;
+
+use pocketpe\pocketpe;
+
+class Player extends PocketPE {
+ >Actions()List>>
+  (OP)
+  (Deop)
+  (GamememodeChange_Creative)
+  (GamemodeChange_Survival)
+  (GamemodeChange_Adventure)
+  (GamemodeChange_Spectator)
+  (ban)
+  (kick)
+  (temp_ban)
+}
