4b3c4f266478352e74fe3c3ee26a63e4830ceb9a
DuncanA <mccleanduncan03@hotmail.com>
2015-08-02 17:09:19 +0100

    Gets the name of player from the packets send from client to server

---
+++ src/PocketPE/player/playerName.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PocketPE\player\playerName;
+
+use pocketpe\pocketpe;
+use pocketpe\player\player;
+
+class playerName extends PocketPE {
+ >Entity>Player>(getName)=PacketSent
+  >usePacketData='playerName'
+}
