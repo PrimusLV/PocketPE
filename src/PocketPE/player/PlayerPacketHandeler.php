3ad61d52f0128065961bde7ec64c4d705f226239
DuncanA <mccleanduncan03@hotmail.com>
2015-08-03 10:18:42 +0100

    Manages the Packets for Players
    This does not include general packets. And to be added..
    Player Languages are there but can not be obtained as

---
+++ src/PocketPE/player/PlayerPacketHandeler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PocketPE\player\player;
+
+use pocketpe\pocketpe;
+use pocketpe\player\player;
+use pocketpe\Packets;
+
+class PlayerPacketHandeler extends PocketPE {
+
+}
+
+class PlayerPacketHandeler extends Packets {
+ >getPacket>Done
+  extend palyerName
+  extend playerIP
+  extend playerTime
+  extend playerLanguage
+  // extend chunks=16 >> block_load
+  
+ //extend all to be given out the system
+}
+class PlayerPacketHandeler extends Player {
+
+}
