2798d5e5f6f696bb0f6b8dbefc40d47cfcc9ff75
DuncanA <mccleanduncan03@hotmail.com>
2015-08-03 16:52:39 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+/* PocketPE
+Server Software for Minecraft Pocket Edition
+Website: pocketpe.esy.es
+If you find issues report them on the Issue Tracker
+*/
+
+ <start> = src\PocketPE\PocketPE.php START
+ <stop> = src\PocketPE\PocketPE.php STOP
+
+//Starting Server
+
+//Stopping Server
+//You can find all the log of the server in your server files. In the log folder. The session name was sesion_%what_session%
