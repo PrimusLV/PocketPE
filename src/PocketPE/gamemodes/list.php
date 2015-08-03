f5fbe9e8ec5fe29fbe504f6ef077f6f98ba3aa77
DuncanA <mccleanduncan03@hotmail.com>
2015-08-03 17:00:58 +0100

    Create list.php

---
+++ src/PocketPE/gamemodes/list.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PocketPE\gamemodes\list;
+
+use pocketpe\pocketpe;
+
+class List extends PocketPE {
+
+ public function List(){
+ 
+  creative
+  survival
+  adventure
+  spectator
+ 
+ }
+
+}
