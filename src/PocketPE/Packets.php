b3c8e68b291f0984d9f0e4c1a8b967524da16884
DuncanA <mccleanduncan03@hotmail.com>
2015-08-05 09:56:44 +0100

    Deals with Packet Collecting

---
+++ src/PocketPE/Packets.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PocketPE\Packets;
+
+use pocketpe\pocketpe;
+use pocketpe\plugins\core;
+use pocketpe\player\player;
+use pocketpe\player\playerpackethandeler;
+use pocketpe\inv\inv;
+use pocketpe\items\items;
+use pocketpe\items\brokenItem;
+use pocketpe\items\placedItem;
+
+class Packets extends PocketPE {
+
+<GetPackets > Client >List(forAll)
+      List.Now>Create
+       get.playerName
+       get.PlayerIP
+       get.playerTime
+       get.playerLanguage
+       // this is a list of the packets that PocketPE gets from the client machine.
+       
+}
