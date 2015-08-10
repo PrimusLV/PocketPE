97471e1c91c226011e412a432602db3fd0d11aec
DuncanA <DunxGit@users.noreply.github.com>
2015-08-10 10:57:14 +0100

    Update and rename Player.php to PlayerActions.php

---
+++ src/PocketPE/actions/PlayerActions.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PocketPE\actions\Player;
+
+use pocketpe\pocketpe;
+
+class Player extends PocketPE {
+ >Actions()List>>
+  (OP) - Oparates the Plugin with more commands
+  (Deop) - Un-Operates player from more commands
+  (GamememodeChange_Creative) - Changes Gamemode
+  (GamemodeChange_Survival) - Changes Gamemode
+  (GamemodeChange_Adventure) - Changes Gamemode
+  (GamemodeChange_Spectator) - Changes Gamemode
+  (ban) - Bans player from being allowed to play on server
+  (kick) - Kicks player from serveer
+  (temp_ban) - temp bans player from playing on server
+}
