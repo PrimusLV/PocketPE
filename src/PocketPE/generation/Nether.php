61f7fd7adcc2bea180c56b68680ae9201339fe6f
DuncanA <DunxGit@users.noreply.github.com>
2015-08-10 10:09:58 +0100

    Create Nether.php

---
+++ src/PocketPE/generation/Nether.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PocketPE\generation\Normal;
+
+use pocketpe\PocketPE;
+
+class Normal extends PocketPE {
+
+ public function Generation() {
+  Server>canGenerateWorld
+   Layer>1>bedrock
+   
+   packetProvide>WorldPackets
+   
+    //use the same world generation as mcpe client and let it write to server disk
+ }
+}
