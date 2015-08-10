9babc6544f24913bb46e48ed4149cc7c45b2f77c
DuncanA <DunxGit@users.noreply.github.com>
2015-08-10 10:27:45 +0100

    Create languages.php

---
+++ src/PocketPE/languages.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PocketPE\languages;
+
+use pocketpe\pocketpe;
+use pocketpe\packets;
+
+class Languages extends PocketPE {
+
+ public function LanguageList() {
+  List>Languages
+   English
+   // we currently only support English
+ }
+}
