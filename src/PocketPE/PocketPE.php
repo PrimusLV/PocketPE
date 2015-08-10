4e24650dd6dec6044131fac92c3151ebc1067722
DuncanA <DunxGit@users.noreply.github.com>
2015-08-10 10:19:42 +0100

    Added packet support and the support for World Gens

--- src/PocketPE/PocketPE.php
+++ src/PocketPE/PocketPE.php
@@ -3,10 +3,20 @@
 namespace PocketPE\PocketPE;
 
 use pocketpe\PocketPE;
+use pocketpe\packets;
+use pocketpe\generation\normal;
+use pocketpe\generation\flat;
+use pocketpe\generation\nether;
 
 class Core extends PocketPE {
- public function PocketPE {
+ public function PocketPE() {
   Main>PocketPE() == nul
     Main>Plugin>PocketPE() == 'PocketPE\plugin\core'
+ }
+ public function Generation() {
+  PacketGenerations>Type>>List
+  PacketGenList.Normal
+  PacketGenList.Flat
+  PacketGenList.Nether
  }
 }
