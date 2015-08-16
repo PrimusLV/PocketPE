ebd23c2ccee1c1c75def4b3e67e6d330b270fb2e
DuncanA <DunxGit@users.noreply.github.com>
2015-08-16 15:41:33 +0100

    Update Invs.php

--- src/PocketPE/gamemodes/Invs.php
+++ src/PocketPE/gamemodes/Invs.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace PocketPE\inv\inv;
+
+use pocketpe\pocketpe;
+use pocketpe\inv\inv;
+use pocketpe\inv\adventureInv;
+use pocketpe\inv\creativeInv;
+use pocketpe\inv\survivalInv;
+use pocketpe\inv\spectatorInv;
+use pocketpe\gamemodes\creative;
+use pocketpe\gamemodes\survival;
+use pocketpe\gamemodes\spectator;
+use pocketpe\gamemodes\adventure;
+use pocketpe\gamemodes\list;
+
+class Invs extends PocketPE {
+}
