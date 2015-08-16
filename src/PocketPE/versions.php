333542b81c3ea90546f64463d7eb981d206fec5b
DuncanA <DunxGit@users.noreply.github.com>
2015-08-16 15:25:21 +0100

    Create versions.php

---
+++ src/PocketPE/versions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PocketPE\PocketPE;
+
+use pocketpe\PocketPE;
+use pocketpe\packets;
+use pocketpe\generation\normal;
+use pocketpe\generation\flat;
+use pocketpe\generation\nether;
+
+class Versions extends PocketPE {
+ 
+ public function Versions() {
+get.clientVersion=0.12.1b10
+ correctVersion.connectYes
+ incorrectVersion.connectNo 
+  if=connectNo>>pingmsg="Client Disconnected because of version."
+ }
+}
