53f16a7e68c2273e1695d55b0cc39b792791f31d
DuncanA <DunxGit@users.noreply.github.com>
2015-08-28 19:07:05 +0100

    Update versions.php

--- src/PocketPE/versions.php
+++ src/PocketPE/versions.php
@@ -9,7 +9,7 @@ class Versions extends PocketPE {
  
  public function Versions() {
 Packets>>playerVersion  
-get.clientVersion=0.12.1b10
+get.clientVersion=0.12.1b12
  correctVersion.connectYes
  incorrectVersion.connectNo 
   if=connectNo>>pingmsg="Client Disconnected because of version."
