d14ea823b3dc282f56f4a059c3f7af050f30316e
howcanjan <dunxgit@users.noreply.github.com>
2015-09-25 06:52:25 +0100

    Created Spawn and gets from apcket

---
+++ src/PocketPE/player/PlayerSpawnEvent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PocketPE\player\PlayerSpawnEvent;
+
+use pocketpe\pocketpe;
+use pocketpe\player\playerName;
+use pocketpe\player\playerpackethandeler;
+
+class PlayerSpawnEvent extends PocketPE {
+
+Packets>Get>PlayerSpawn
+        corrds_x>''
+        coords_y>''
+        coords-z>''
+
+}
