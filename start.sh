bb9209f458ef1667be22fa35436fb4c794f57ae6
howcanjan <dunxgit@users.noreply.github.com>
2015-09-27 09:15:41 +0100

    Added Crash and No corupt

--- start.sh
+++ start.sh
@@ -4,10 +4,15 @@ Website: pocketpe.esy.es
 If you find issues report them on the Issue Tracker
 */
 
- <start> = src\PocketPE\PocketPE.php START
- <stop> = src\PocketPE\PocketPE.php STOP
+//Loader
+//Load when <file_exacute>
+   <start> = src\PocketPE\PocketPE.php START
+   <stop> = src\PocketPE\PocketPE.php STOP
 
 //Starting Server
 
 //Stopping Server
-//You can find all the log of the server in your server files. In the log folder. The session name was sesion_%what_session%
+//You can find all the log of the server in your server files. In the log folder. The session name was sesion_%what_session%#
+
+//Crash
+//Ohs Nos, we have crashed. Check all Binaries and code in plugins.
